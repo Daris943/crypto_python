f7c881401582c870f1d730ca7993f31517642505
Daris943 <danil.f.haris@gmail.com>
2023-11-08 16:30:31 +0100

    add order script

--- filters_key_by_size.py
+++ filters_key_by_size.py
@@ -19,8 +19,11 @@ for file_name in files:
         certs.append([file_name, key_length])
         cpt += 1
 
-        if cpt == 200:
-            break
+        if cpt % 20000 == 0:
+            print("cpt = ", cpt)
+
+print("--------------")
+print("finish to load certificates")
 
 # apply a bubble sort algo
 n = len(certs)
@@ -28,13 +31,33 @@ for i in range(n):
     for j in range(0, n - i - 1):
         if certs[j][1] > certs[j + 1][1]:
             certs[j], certs[j + 1] = certs[j + 1], certs[j]
+    if i % 1000 == 0:
+        print("tri : ", i)
+print("end of the bubble sort")
+
+# apply a quick sort
+if len(certs) <= 1:
+    pass
+else:
+    pivot = certs[0]
+    less = [cert for cert in certs[1:] if cert[1] <= pivot[1]]
+    greater = [cert for cert in certs[1:] if cert[1] > pivot[1]]
+    sorted_certs = less + [pivot] + greater
+print("quick sort finish")
 
 # copy order in a txt
 f = open("sorted_order.txt", 'w')
 
+cpt_copy = 0
+
 # copy certificates in a new dir after sort
-for filename, key_length in certs:
-    source_path = os.path.join(directory_path, filename)
-    dest_path = os.path.join(directory_path_sorted, filename)
-    shutil.copyfile(source_path, dest_path)
+for filename, key_length in sorted_certs:  # certs:
+    # source_path = os.path.join(directory_path, filename)
+    # dest_path = os.path.join(directory_path_sorted, filename)
+    # shutil.copyfile(source_path, dest_path)
     f.write(filename + ", " + str(key_length) + "\n")
+
+    cpt_copy += 1
+
+    if cpt_copy % 5000 == 0:
+        print("insert number : ", cpt_copy)
